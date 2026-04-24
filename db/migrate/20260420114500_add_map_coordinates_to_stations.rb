8d2ef2a2c06982c3b491d57157bff587c8d8214f
marcobeffa <marcobeffa@gmail.com>
2026-04-24 22:56:21 +0200

    fix database

--- db/migrate/20260420114500_add_map_coordinates_to_stations.rb
+++ db/migrate/20260420114500_add_map_coordinates_to_stations.rb
@@ -1,6 +1,6 @@
 class AddMapCoordinatesToStations < ActiveRecord::Migration[8.1]
   def change
-    add_column :stations, :map_x, :integer
-    add_column :stations, :map_y, :integer
+    add_column :stations, :map_x, :integer unless column_exists?(:stations, :map_x)
+    add_column :stations, :map_y, :integer unless column_exists?(:stations, :map_y)
   end
 end
